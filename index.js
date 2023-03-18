a19b458fc6f687c87994359e7653661f45ae81f3
Ritvij <kritvij000@gmail.com>
2023-03-18 16:09:08 +0100

    updated port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const db = require('./queries');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
